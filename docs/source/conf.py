3606ad1ad6757308cc1043ba4384added5054a30
nirleev <50886301+nirleev@users.noreply.github.com>
2022-06-10 18:56:40 +0200

    Update conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -2,9 +2,9 @@
 
 # -- Project information
 
-project = 'Lumache'
-copyright = '2021, Graziella'
-author = 'Graziella'
+project = 'A low-power nuclear reactor controller'
+copyright = '2022, nirleev'
+author = 'nirleev'
 
 release = '0.1'
 version = '0.1.0'
@@ -20,7 +20,6 @@ extensions = [
 ]
 
 intersphinx_mapping = {
-    'python': ('https://docs.python.org/3/', None),
     'sphinx': ('https://www.sphinx-doc.org/en/master/', None),
 }
 intersphinx_disabled_domains = ['std']
